acbb50feffc5f5fcd7bccc2a4c590efab30f75e0
Erik Rutledge <104602743+erikrutledge@users.noreply.github.com>
2023-06-09 16:47:43 -0600

    Uploaded the formatted home page and week1 work

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+let date = new Date()
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+
+let fullDate = `${day}/${month}/${year}`;
+
+document.getElementById('year').innerHTML = `&copy${year} | Erik Rutledge <br> United States of America`
+document.getElementById('lastModified').innerHTML = `Last Modified ${fullDate}`
