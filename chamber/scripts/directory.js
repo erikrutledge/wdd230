c0570546acedc024083f88dfc750e371ed300a6a
Erik Rutledge <104602743+erikrutledge@users.noreply.github.com>
2023-07-08 21:00:27 -0600

    Completed the week 5 chamber project updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,17 +1,20 @@
-const linksURL = "https://erikrutledge.github.io/wdd230/chamber/data/members.json";
-const destination = document.querySelector('#content');
+const membersURL = "https://erikrutledge.github.io/wdd230/chamber/data/members.json";
+
+const gridDest = document.querySelector('#grid');
+const listDest = document.querySelector('#table');
 
 async function getMembers() {
-    const response = await fetch("../data/members.json"); // linksURL
+    const response = await fetch(membersURL);
     const data = await response.json();
-    displayGrid(data.members);
+    createElements(data.members);
+    displayGrid();
 }
 
-function displayGrid(members) {
+function createElements(members) {
     members.forEach(member => {
-        // Create all the grid elements
-        const card = document.createElement("section");
-
+        // GRID CONTENT
+        // Create all the elements
+        const card = document.createElement("div");
         const icon = document.createElement("img");
         const address = document.createElement("p");
         const phone = document.createElement("p");
@@ -23,6 +26,7 @@ function displayGrid(members) {
         address.textContent = member.address;
         phone.textContent = member.phone;
         website.setAttribute('href', member.website);
+        website.textContent = member.website;
 
         // Add the elements to the card
         card.appendChild(icon);
@@ -31,11 +35,42 @@ function displayGrid(members) {
         card.appendChild(website);
 
         // Add card to the content section of the page 
-        destination.appendChild(card);
-    });
+        gridDest.appendChild(card);
+
+        // LIST CONTENT
+        // Create elements to fill the table
+        const row = document.createElement("tr");
+        const c1 = document.createElement("td")
+        const c2 = document.createElement("td")
+        const c3 = document.createElement("td")
+        const c4 = document.createElement("td")
+
+        c1.textContent = member.name;
+        c2.textContent = member.phone;
+        c3.textContent = member.membership;
+        const webpage = document.createElement("a");
+        webpage.setAttribute('href', member.website);
+        webpage.textContent = "More Details";
+        c4.appendChild(webpage);
+
+        row.appendChild(c1);
+        row.appendChild(c2);
+        row.appendChild(c3);
+        row.appendChild(c4);
+
+        listDest.appendChild(row);
+    })
+}
+
+function displayGrid() {
+    document.getElementById("grid").style.display = "";
+    document.getElementById("table").style.display = "none";
+
+}
+
+function displayTable() {
+    document.getElementById("table").style.display = "";
+    document.getElementById("grid").style.display = "none";
+
 }
 getMembers();
-
-function displayList(data) {
-
-}
